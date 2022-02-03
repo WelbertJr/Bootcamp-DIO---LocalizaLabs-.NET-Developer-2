c768d1c0799b49790da010cfd759382c1f7037a0
Welbert Junior <81397022+WelbertJr@users.noreply.github.com>
2022-02-03 20:51:16 -0300

    Add file PrimeiroProjetoCSharp

---
+++ PrimeiroProjetoCSharp/PrimeiroProjetoCSharp/Classe.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrimeiroProjetoCSharp
+{
+    class Pessoa
+    {
+        public string Nome { get; set; }
+        public string Estado { get; set; }
+        public int Idade { get; set; }
+        
+    }
+}
---
+++ PrimeiroProjetoCSharp/PrimeiroProjetoCSharp/Namespace.cs
@@ -0,0 +1,16 @@
+﻿namespace Primeiro
+{
+    class Classe
+    {
+
+    }
+}
+
+namespace Segundo
+{
+    class Classe 
+    {
+
+    }
+
+}
